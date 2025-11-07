5a4e35f0065b6b51a88ed7242782a5b1da2bcc17
Mikhail Tsuker <Mikhail.Tsuker@internationalsos.com>
2025-11-07 15:00:11 -0500

    add lanf smith

--- main.py
+++ main.py
@@ -1,15 +1,41 @@
 import os
 
 from dotenv import load_dotenv
+from langchain_core.prompts import PromptTemplate
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_ollama import ChatOllama
 
 load_dotenv()
 comment: str = "This is a langchain-course project."
 
 
 def main():
-    print("Hello from langchain-course!")
-    print("Your Google API Key is:", os.getenv("GOOGLE_API_KEY"))
-    print(os.environ.get("GOOGLE_API_KEY"))
+
+    information = """
+Elon Reeve Musk[b] (born June 28, 1971) is a businessman and entrepreneur known for his leadership of Tesla, SpaceX, Twitter, and xAI. Musk has been the wealthiest person in the world since 2021; as of October 2025, Forbes estimates his net worth to be around $500 billion.
+
+Born into a wealthy family in Pretoria, South Africa, Musk emigrated in 1989 to Canada; he had obtained Canadian citizenship at birth through his Canadian-born mother. He received bachelor's degrees in 1997 from the University of Pennsylvania in Philadelphia, United States, before moving to California to pursue business ventures. In 1995, Musk co-founded the software company Zip2. Following its sale in 1999, he co-founded X.com, an online payment company that later merged to form PayPal, which was acquired by eBay in 2002. That year, Musk also became an American citizen.
+
+In 2002, Musk founded the space technology company SpaceX, becoming its CEO and chief engineer; the company has since led innovations in reusable rockets and commercial spaceflight. Musk joined the automaker Tesla as an early investor in 2004 and became its CEO and product architect in 2008; it has since become a leader in electric vehicles. In 2015, he co-founded OpenAI to advance artificial intelligence (AI) research, but later left; growing discontent with the organization's direction and their leadership in the AI boom in the 2020s led him to establish xAI. In 2022, he acquired the social network Twitter, implementing significant changes, and rebranding it as X in 2023. His other businesses include the neurotechnology company Neuralink, which he co-founded in 2016, and the tunneling company the Boring Company, which he founded in 2017.
+"""
+    summary_template = """
+given the information {information}, provide 
+1. short summary about the person in less than 100 words.
+2. two interesting facts about him.
+"""
+
+    summary_prompt_template = PromptTemplate(
+    input_variables=["information"], template=summary_template
+)
+
+    #llm = ChatGoogleGenerativeAI(model="gemini-2.5-flash", temperature=0)
+    llm = ChatOllama(model="gemma3:4b", temperature=0)
+    # llm = ChatOllama(model="gpt-oss:20b", temperature=0)
+    chain = summary_prompt_template | llm
+    response = chain.invoke ({"information": information})
+
+    print(response.content)    
+
 
 
 if __name__ == "__main__":
